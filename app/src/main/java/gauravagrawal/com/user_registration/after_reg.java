a901a81fd749102082fa9061c887b46ec908dcea
Gaurav Agrawal <gauravagrawal223@gmail.com>
2016-03-05 22:44:38 +0530

    Alert Dialog Box added

--- app/src/main/java/gauravagrawal/com/user_registration/after_reg.java
+++ app/src/main/java/gauravagrawal/com/user_registration/after_reg.java
@@ -1,5 +1,7 @@
 package gauravagrawal.com.user_registration;
 
+import android.app.AlertDialog;
+import android.content.DialogInterface;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.widget.CompoundButton;
@@ -19,10 +21,29 @@ public class after_reg extends AppCompatActivity {
             public void onCheckedChanged(CompoundButton compoundButton, boolean isChecked) {
                 if(isChecked){
                     Toast.makeText(getApplicationContext(),"Switch On",Toast.LENGTH_LONG).show();
+                    alertdialog();
                 }
                 else
                     Toast.makeText(getApplicationContext(),"Switch off",Toast.LENGTH_LONG).show();
             }
         });
+    }
+    void alertdialog(){
+        AlertDialog.Builder alertbox=new AlertDialog.Builder(after_reg.this);
+        alertbox.setTitle("Notify Services..");
+        alertbox.setMessage("You sure you need assistance for the accident?");
+        alertbox.setPositiveButton("YES", new DialogInterface.OnClickListener() {
+            @Override
+            public void onClick(DialogInterface dialogInterface, int i) {
+                Toast.makeText(getApplicationContext(), "You Clicked Yes", Toast.LENGTH_LONG).show();
+            }
+        });
+        alertbox.setNegativeButton("NO", new DialogInterface.OnClickListener() {
+            @Override
+            public void onClick(DialogInterface dialogInterface, int i) {
+                Toast.makeText(getApplicationContext(),"You Clicked No",Toast.LENGTH_LONG).show();
+            }
+        });
+        alertbox.show();
     }
 }
